b423d9a2819a11a5c7fc9d9980c916dfec564740
HauXun <hautrieu65@gmail.com>
2022-02-24 22:26:19 +0700

    custom context menu

---
+++ Skills Overview/JavaScript Training/Coding Artist/Javascript Tutorials/Custom Context Menu/script.js
@@ -0,0 +1,78 @@
+// Events for desktop and touch
+let events = ["contextmenu", "touchstart"];
+// Initial declaration
+var timeout;
+// for double tap
+var lastTap = 0;
+// refer menu div
+let contextMenu = document.getElementById('context-menu');
+
+// same function for both events
+events.forEach((eventType) => {
+    document.addEventListener(eventType, (e) => {
+        e.preventDefault();
+        // x and y position of mouse or touch
+        let mouseX = e.clientX || e.touches[0].clientX;
+        let mouseY = e.clientY || e.touches[0].clientY;
+
+        // height and width of menu
+        let menuHeight = contextMenu.getBoundingClientRect().height;
+        let menuWidth = contextMenu.getBoundingClientRect().width;
+
+        // width and height of screen
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+
+        // If user clicks/touches near right corner
+        if (width - mouseX <= 200) {
+            contextMenu.style.borderRadius = "5px 0 5px 5px";
+            contextMenu.style.left = width - menuWidth + "px";
+            contextMenu.style.top = mouseY + "px";
+
+            // right bottom
+            if (height - mouseY <= 200) {
+                contextMenu.style.top = mouseY - menuHeight + "px";
+                contextMenu.style.borderRadius = "5px 5px 0 5px";
+            }
+        }
+        // left
+        else {
+            contextMenu.style.borderRadius = "0 5px 5px 5px";
+            contextMenu.style.left = mouseX + "px";
+            contextMenu.style.top = mouseY + "px";
+
+            // left bottom
+            if (height - mouseY <= 200) {
+                contextMenu.style.top = mouseY - menuHeight + "px";
+                contextMenu.style.borderRadius = "5px 5px 5px 0";
+            }
+        }
+        // display the menu
+        contextMenu.style.visibility = "visible";
+    }, {
+        passive: false
+    });
+});
+
+// for double tap (works on touch device)
+document.addEventListener("touchend", function(e) {
+    // current time
+    var currentTime = new Date().getTime();
+    // gap between two gaps
+    var tapLength = currentTime - lastTap;
+    // clear previous timeouts(if any exist)
+    clearTimeout(timeout);
+    // if user taps twice in 500ms
+    if (tapLength < 500 && tapLength > 0) {
+        // hide menu
+        contextMenu.style.visibility = "hidden";
+        e.preventDefault();
+    } else {
+        // timeout if user doesn't tap after 500ms
+        timeout = setTimeout(function() {
+            clearTimeout(timeout);
+        }, 500);
+    }
+    // lastTap set to current time
+    lastTap = currentTime;
+});
