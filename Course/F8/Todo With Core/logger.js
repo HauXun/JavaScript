f8906b4a2ea4c4f87431bc24a397abcf3aab6663
HauXun <hautrieu65@gmail.com>
2022-07-18 22:19:50 -0700

    todo app with core

---
+++ Course/F8/Todo With Core/logger.js
@@ -0,0 +1,11 @@
+export default function logger(reducer) {
+  return (preState, action, args) => {
+    console.group(action);
+    console.log("Prev State: ", preState);
+    console.log("Action Arguments: ", args);
+    const nextState = reducer(preState, action, args);
+    console.log("Next State: ", nextState);
+    console.groupEnd(action);
+    return nextState;
+  };
+}
